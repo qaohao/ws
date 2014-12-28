4f525adc66f2ca242d6226dffdff7820ad4f9565
meng03_zhang <meng03_zhang@allyes.com>
2014-12-28 16:41:45 +0800

    add subuser mybatis mapping

---
+++ src/main/java/com/allyes/weimax/model/Subuser.java
@@ -0,0 +1,32 @@
+package com.allyes.weimax.model;
+
+import java.util.Date;
+
+/**
+ * 公众平台粉丝表。
+ * 
+ * @author qaohao
+ */
+public class Subuser {
+	private long id;
+	private long wxServiceAccountId;
+	private String unionOpenId;
+	private String openId;
+	private String hashOpenId;
+	private String unionId;
+	private int isSubscribed;
+	private String nickname;
+	private int sex;
+	private String language;
+	private String city;
+	private String province;
+	private String country;
+	private String headImgUrl;
+	private long subscribeTime;
+	private Date lastUpadte;
+	private String channelId;
+	private String RealLocation;
+	private String groupId;
+	private String address;
+	private String note;
+}
